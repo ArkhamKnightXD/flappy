f597d89025f5088589226170a25c9ae763cdac5d
knight <karvin_jimenez@hotmail.com>
2023-12-13 20:18:01 -0400

    extra changes.

--- core/src/main/java/knight/arkham/objects/Player.java
+++ core/src/main/java/knight/arkham/objects/Player.java
@@ -24,8 +24,6 @@ public class Player extends GameObject {
             world, "yellowbird-midflap.png", "wing.wav"
         );
 
-        score = 0;
-
         TextureAtlas atlas = new TextureAtlas("images/birds.atlas");
 
         TextureRegion region = atlas.findRegion("yellow-bird");
